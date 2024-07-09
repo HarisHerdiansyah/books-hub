7b119305006e8dbbe0fab784173ff41a09da337f
Haris Herdiansyah <haris54237@gmail.com>
2024-07-09 12:36:01 +0700

    write auth boilerplate

--- src/pages/auth/login/Login.jsx
+++ src/pages/auth/login/Login.jsx
@@ -1,5 +1,5 @@
 import { useNavigate } from 'react-router-dom';
-import { mockAuth } from '../../../constants/functions';
+import { login } from '../../../service/auth';
 import { PATH } from '../../../constants/routes';
 import { Form } from '../../../components/global';
 import { AuthLink } from '../../../components/auth';
@@ -22,16 +22,7 @@ export default function Login() {
   const handleLogin = async (e) => {
     console.log('first');
     e.preventDefault();
-    try {
-      const path = await mockAuth.login({
-        username: 'Admin#1234',
-        password: 'Admin#1234'
-      });
-      navigate(path);
-      window.location.reload();
-    } catch (error) {
-      console.error('GAGAL LOGIN', error);
-    }
+    await login();
   };
 
   const toRegister = () => navigate(PATH.auth.register);
