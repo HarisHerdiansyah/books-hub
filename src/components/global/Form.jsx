ddae3aa923d1a1476a3886fafc6869d03999253e
Haris Herdiansyah <haris54237@gmail.com>
2024-07-12 07:55:25 +0700

    feat: add new form field

--- src/components/global/Form.jsx
+++ src/components/global/Form.jsx
@@ -4,7 +4,11 @@ import {
   Input,
   FormLabel,
   Select,
-  Textarea
+  Textarea,
+  RadioGroup,
+  Radio,
+  Stack,
+  Checkbox
 } from '@chakra-ui/react';
 
 export default function Form({
@@ -14,6 +18,9 @@ export default function Form({
   id,
   placeholder,
   optData,
+  value,
+  onChange,
+  stackDirection,
   ...props
 }) {
   if (type === 'select') {
@@ -30,6 +37,36 @@ export default function Form({
           ))}
         </Select>
       </FormControl>
+    );
+  }
+
+  if (type === 'radio-group') {
+    return (
+      <RadioGroup onChange={onChange} value={value}>
+        <Stack direction={stackDirection}>
+          {optData.map((opt) => (
+            <Radio value={opt.value} key={opt.label}>
+              {opt.label}
+            </Radio>
+          ))}
+        </Stack>
+      </RadioGroup>
+    );
+  }
+
+  if (type === 'radio') {
+    return (
+      <Radio value={value} onChange={onChange}>
+        {label}
+      </Radio>
+    );
+  }
+
+  if (type === 'checkbox') {
+    return (
+      <Checkbox id={id} value={value} onChange={onChange}>
+        {label}
+      </Checkbox>
     );
   }
 
@@ -53,5 +90,8 @@ Form.propTypes = {
   placeholder: PropTypes.string,
   isRequired: PropTypes.bool,
   id: PropTypes.string.isRequired,
-  optData: PropTypes.array
+  optData: PropTypes.array,
+  value: PropTypes.string,
+  onChange: PropTypes.func,
+  stackDirection: PropTypes.string
 };
--- src/pages/book/form/BookForm.jsx
+++ src/pages/book/form/BookForm.jsx
@@ -5,16 +5,22 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faStar as faStarReg } from '@fortawesome/free-regular-svg-icons';
 import { faStar as faStarSolid } from '@fortawesome/free-solid-svg-icons';
 import { GlobalComponent } from '../../../components';
+import { utils } from '../../../constants';
 
 export default function BookForm() {
   const { pathname } = useLocation();
   const navigate = useNavigate();
+
   const isEdit = pathname.includes('edit');
   const title = isEdit ? 'Perbarui Buku' : 'Tambah Buku';
   const ratingIcon = {
     empty: <FontAwesomeIcon size='xl' color='red' icon={faStarReg} />,
     full: <FontAwesomeIcon size='xl' color='red' icon={faStarSolid} />
   };
+  const dropdownCategory = utils.bookCategoryDropdown.map((cat) => ({
+    value: cat,
+    label: cat
+  }));
 
   const handleBack = () => {
     navigate(-1);
@@ -47,30 +53,46 @@ export default function BookForm() {
           />
           <GlobalComponent.GridForm
             type='select'
-            label='Genre'
-            id='genre'
-            optData={[
-              { value: 'Genre 1', label: 'Genre 1' },
-              { value: 'Genre 2', label: 'Genre 2' }
-            ]}
+            label='Kategori'
+            id='category'
+            optData={dropdownCategory}
             isRequired
           />
-        </Grid>
-        <Flex align='center' my={6} gap={6}>
-          <Text fontSize='xl' fontWeight='semibold'>
-            Rating:
-          </Text>
-          <Rating
-            fractions={2}
-            emptySymbol={ratingIcon.empty}
-            fullSymbol={ratingIcon.full}
+          <GlobalComponent.GridForm
+            type='radio-group'
+            value='Privat'
+            stackDirection='column'
+            optData={[
+              { value: 'Privat', label: 'Privat' },
+              { value: 'Publik', label: 'Publik' }
+            ]}
           />
-        </Flex>
-        <GlobalComponent.Form
-          type='textarea'
-          id='descAndReview'
-          label='Deskripsi dan Review'
-        />
+          <GlobalComponent.GridForm
+            type='checkbox'
+            label='Tambah ke wishlist'
+            id='wishlist'
+            value={true}
+          />
+        </Grid>
+        {isEdit && (
+          <>
+            <Flex align='center' my={6} gap={6}>
+              <Text fontSize='xl' fontWeight='semibold'>
+                Rating:
+              </Text>
+              <Rating
+                fractions={2}
+                emptySymbol={ratingIcon.empty}
+                fullSymbol={ratingIcon.full}
+              />
+            </Flex>
+            <GlobalComponent.Form
+              type='textarea'
+              id='descAndReview'
+              label='Deskripsi dan Review'
+            />
+          </>
+        )}
         <Flex align='center' justify='space-between' mt={12}>
           <Button colorScheme='red' variant='outline' onClick={handleBack}>
             Batalkan
