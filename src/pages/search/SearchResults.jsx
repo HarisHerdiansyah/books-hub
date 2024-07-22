aec8d027ab73b79abb99df82a18705a3b8d53294
Haris Herdiansyah <haris54237@gmail.com>
2024-07-22 08:07:04 +0700

    feat: implement card component in search-results page

--- src/pages/search/SearchResults.jsx
+++ src/pages/search/SearchResults.jsx
@@ -1,50 +1,15 @@
-import { Link } from 'react-router-dom';
-import { PATH } from '../../constants';
-import {
-  Button,
-  Card,
-  CardBody,
-  Container,
-  Text,
-  Grid,
-  GridItem
-} from '@chakra-ui/react';
+import { Container, Text, Grid, GridItem } from '@chakra-ui/react';
+import { SearchComponent } from '../../components';
 
 export default function SearchResults() {
   return (
-    <Container maxW={900}>
+    <Container maxW={1000}>
       <Text fontSize='3xl' my={6}>
         Hasil Pencarian : Negeri 5 Menara
       </Text>
       <Grid templateColumns='repeat(2, 1fr)' gap={6} my={6}>
         <GridItem>
-          <Card variant='outline' w='100%'>
-            <CardBody>
-              <Link to={PATH.book.detail}>
-                <Button colorScheme='blue' variant='link' fontSize='xl' my={2}>
-                  @username/judul buku
-                </Button>
-              </Link>
-              <Text my={2}>Tanggal publikasi: 24 Maret 2024</Text>
-              <Text my={2}>Total kunjungan: 298 kali</Text>
-              <Link to={PATH.showcase.overview}>
-                <Button colorScheme='pink' variant='link'>
-                  Lihat Profil
-                </Button>
-              </Link>
-            </CardBody>
-          </Card>
-        </GridItem>
-        <GridItem>
-          <Card variant='outline' w='100%'>
-            <CardBody>
-              <Button colorScheme='blue' variant='link' fontSize='xl' my={2}>
-                @username/judul buku
-              </Button>
-              <Text my={2}>Tanggal publikasi: 24 Maret 2024</Text>
-              <Text my={2}>Total kunjungan: 298 kali</Text>
-            </CardBody>
-          </Card>
+          <SearchComponent.ResultCard />
         </GridItem>
       </Grid>
     </Container>
